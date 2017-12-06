883c15140c17b4e701b754eac99b4cee8e9562a6
satyabrata <s.rakshit88@gmail.com>
2017-12-06 09:07:08 +0530

    no message

--- vidjot/config/database.js
+++
@@ -1,5 +0,0 @@
-if(process.env.NODE_ENV === 'production'){
-    module.exports = {mongoURI : 'mongodb://satya1234:satya1234@ds129796.mlab.com:29796/ideavid-prod'}  //cloud database
-} else {
-    module.exports = {mongoURI : 'mongodb://localhost/vidjot-dev'}  //local database
-}
