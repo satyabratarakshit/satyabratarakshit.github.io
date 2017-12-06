15b2ae5841a0a88deeddb78f98abab0492e9faed
satyabrata <s.rakshit88@gmail.com>
2017-12-06 09:39:56 +0530

    storybook application

---
+++ storybooks/config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI:'mongodb://satya1234:satya1234@ds129966.mlab.com:29966/storybooks-dev',
+    googleClientID:'109564532653-b50ohfc9483m9hufo791dh85njppte7f.apps.googleusercontent.com',
+    googleClientSecret:'HHfGouQsUPpxF-daZBy-6c82'
+}
